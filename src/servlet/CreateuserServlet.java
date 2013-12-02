a116391c066a797916dbf68fc2ae5babb9084689
yunajing <yuna.jing260@gmail.com>
2013-12-01 21:16:50 -0500

    Working CreateUserservlet added

--- src/servlet/CreateuserServlet.java
+++ src/servlet/CreateuserServlet.java
@@ -1,14 +1,13 @@
 package servlet;
 
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@SuppressWarnings("serial")
 public class CreateuserServlet extends HttpServlet{
 	
 	public CreateuserServlet(){
@@ -20,12 +19,7 @@ public class CreateuserServlet extends HttpServlet{
 		String userName = request.getParameter("UserEmail");
 		String password = request.getParameter("passWord");
 		System.out.println(userName+password);
-		
+		getServletContext().getRequestDispatcher("/Asyouwish.html").forward(
+				request, response);
 	}
-	
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException{
-			
-	}
-
 }
